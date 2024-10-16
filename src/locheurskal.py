ca4f47154e7dc2764aca9563bf68e0e3bd1858c0
Axel Glöckner <axel.glockner00@gmail.com>
2024-10-16 11:02:27 +0200

    opt loop finished

--- src/locheurskal.py
+++ src/locheurskal.py
@@ -70,8 +70,25 @@ while sum(rem_demand)>0:
         if shipping_cost[selected_facility][customer_index] < min_shipping_cost:
             min_shipping_cost = shipping_cost[selected_facility][customer_index]
             selected_customer = customer_index
+    
+    #Allocate demand and update Capacities.
+    shipping_cost[selected_facility][selected_customer] = np.inf # ensures it is not chosen again.
+    facility_status[selected_facility] = 1 # facility is open.
+    allocated_amount = min(rem_capacity[selected_facility], rem_demand[selected_customer]) # determine the amount to be allocated.
+    alloc_matrix[selected_facility, selected_customer] = allocated_amount # update allocation matrix.
 
+    # Update remaining capacity and demand.
+    if rem_capacity[selected_facility] >= rem_demand[selected_customer]: # The facility’s capacity is reduced by the amount of demand it fulfilled.
+        rem_capacity[selected_facility] -= int(rem_demand[selected_customer])
+        rem_demand[selected_customer] = 0 # indicating that all their demand has been met.
+        if rem_capacity[selected_facility] == 0:
+            facility_cost[selected_facility] = -1 # If the facility’s capacity is now zero, its cost is set to -1. Not going to be used in other iterations.
+    else:
+        rem_demand[selected_customer] -= rem_capacity[selected_facility] # The remaining demand of the customer is reduced by the facility’s available capacity.
+        rem_capacity[selected_facility] = 0 # Capacity set to zero, has been fully used.
+        facility_cost[selected_facility] = -1 # Exclude it from further iterations.
 
+    num_iterations += 1
 
 elapsed = time.time() - t1
 print('Tid: '+str('%.4f' % elapsed))
